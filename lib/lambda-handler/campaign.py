2ecd22dfb87e56fe2c3908e1a6e7bd03f687f8fd
jakemraz <jakemraz100@gmail.com>
2020-05-18 17:56:17 +0900

    redirect page

--- lib/lambda-handler/campaign.py
+++ lib/lambda-handler/campaign.py
@@ -6,43 +6,31 @@ import boto3
  
 LOG = logging.getLogger()
 LOG.setLevel(logging.INFO)
- 
- 
+
 def handler(event, context):
     LOG.info("EVENT: " + json.dumps(event))
- 
+
     if event['body'] is not None:
         body = json.loads(event['body'])
     else:
         body = event
-    
-    # campaign_id = body['campaignId']
-    # redirect_url = body['redirectUrl']
 
     if event['pathParameters'] is not None:
         return redirect(event)
-    
-    
-    return create_short_url(body)
 
-    return {
-        'statusCode': 200,
-        'body': 'GET url/1/userId'
-    }
- 
+    return create_short_url(body)
  
 def create_short_url(body):
     # Pull out the DynamoDB table name from environment
     table_name = os.environ.get('TABLE_NAME')
- 
+
     # Parse targetUrl
     redirect_url = body['redirectUrl']
     campaign_id = body['campaignId']
- 
+
     ddb = boto3.resource('dynamodb')
     table = ddb.Table(table_name)
     response = table.scan()
-
     item_count = int(response['Count'])
 
     # Create a unique id
@@ -53,34 +41,31 @@ def create_short_url(body):
         'campaign_id': campaign_id
     }
 
-    
     # Create item in DynamoDB
     response = table.put_item(
         Item=item
     )
-  
-    print(response)
 
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'text/plain'},
-        'body': response
+        'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(item)
     }
  
  
 def redirect(event):
     # Parse redirect ID from path
     id = int(event['pathParameters']['proxy'])
- 
+
     # Pull out the DynamoDB table name from the environment
     table_name = os.environ.get('TABLE_NAME')
- 
+
     # Load redirect target from DynamoDB
     ddb = boto3.resource('dynamodb')
     table = ddb.Table(table_name)
     response = table.get_item(Key={'id': id})
-    LOG.debug("RESPONSE: " + json.dumps(response))
- 
+    print(response)
+
     item = response.get('Item', None)
     if item is None:
         return {
@@ -88,11 +73,15 @@ def redirect(event):
             'headers': {'Content-Type': 'text/plain'},
             'body': 'No redirect found for ' + id
         }
- 
+
+    redirect_url = item.get('redirect_url')
+    if redirect_url[:4] != "http":
+        redirect_url = "http://" + redirect_url
+
     # Respond with a redirect
     return {
         'statusCode': 301,
         'headers': {
-            'Location': item.get('redirect_url')
+            'Location': redirect_url
         }
     }
